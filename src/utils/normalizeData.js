3c1617fc6b2cd61209b9d95629ea3545be74f144
Shin <shin4895@gmail.com>
2023-05-21 14:08:24 +0900

    normalizeMallPrice 숫자가아닐때 로직 작성
    지정된 플랫폼단어 다음이 숫자가아닌경우가
    발견, 코드 재작성

--- src/utils/normalizeData.js
+++ src/utils/normalizeData.js
@@ -1,24 +1,22 @@
 
 export const normalizeMallPrice = (text) => {
-  let allowedShops = ["쿠팡", "G마켓", "11번가","옥션","SSG닷컴"]; 
+  let allowedShops = ["쿠팡", "G마켓", "11번가", "옥션", "SSG닷컴"];
   let words = text.split("\n");
-  let shopPrice={};
-  if (!text) {
-    return 0; // or whatever default value you want
-  }
+  let shopPrice = {};
 
-  for(let i=0; words.length>i; i++){
-    if(allowedShops.includes(words[i])){
-      let price = words[i+1].replace(/,/g,''); //가격가져오고 ',' 삭제
-      shopPrice[words[i]]=parseInt(price,10); //10진수
+  for (let i = 0; i < words.length - 1; i++) {
+    if (allowedShops.includes(words[i])) {
+      const nextWord = words[i + 1];
+      if (!isNaN(parseFloat(nextWord.replace(/,/g, '')))) {
+        const price = nextWord.replace(/,/g, '');
+        shopPrice[words[i]] = parseInt(price, 10);
+      }
     }
   }
-  
+
   return shopPrice;
-      
-  
-  ;
 }
+
 
 
 export const normalizeMinPrice = (text) => {
