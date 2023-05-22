2cedafcb3949e187e75c2161b7347b0042279b8f
Shin <shin4895@gmail.com>
2023-05-22 22:21:24 +0900

    item components CSS

--- src/components/productItem.js
+++ src/components/productItem.js
@@ -1,23 +1,49 @@
+import styled from 'styled-components';
+
+const ItemContainer = styled.div`
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ddd;
+`;
+
+const ItemLink = styled.a`
+  text-decoration: none;
+  color: #333;
+`;
+
+const ItemName = styled.p`
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+const ItemPrice = styled.p`
+  margin: 5px 0;
+`;
+
+const ItemProfit = styled.p`
+  color: green;
+  margin: 5px 0;
+`;
+
 
 
 export function ProductItem({ item }) {
-
   return (
-    <div>
-      <a href={item.Link} target='_blank' rel="noopener noreferrer" >
-        <p>Name: {item.name}</p>
-      </a>
+    <ItemContainer>
+      <ItemLink href={item.Link} target='_blank' rel="noopener noreferrer" >
+        <ItemName>Name: {item.name}</ItemName>
+      </ItemLink>
       <div>
       {Object.entries(item.mallPrice).map(([shop, price]) => (
-          <p key={shop}> {price[0]}:{price[1]} 원</p>
+          <ItemPrice key={shop}> {price[0]}:{price[1]} 원</ItemPrice>
         ))}
       </div>
-      <p>Min Price: {item.MinPrice}원</p>
+      <ItemPrice>Min Price: {item.MinPrice}원</ItemPrice>
       {Object.entries(item.mallPrice).map(([shop, price]) => (
-          <p key={shop}> {price[0]}:{price[1]-item.MinPrice} 원 이익</p>
+          <ItemProfit key={shop}> {price[0]}:{price[1]-item.MinPrice} 원 이익</ItemProfit>
         ))}
       
-      <p>-----------------------------------------------</p>
-    </div>
+
+    </ItemContainer>
   );
 }
