210052970fe8ddce78df4693cbb943158c259e6d
Shin <shin4895@gmail.com>
2023-05-26 22:57:50 +0900

    코드 전반적인 리팩토링

--- src/pages/api/[id].js
+++ src/pages/api/[id].js
@@ -8,15 +8,15 @@ import {
   ProfitProduct,
 } from "../../utils/dataProcessing";
 
-function UpdateURL(keyword, pageIndex) {
+const UpdateURL = (keyword, pageIndex) => {
   const url = `https://search.shopping.naver.com/search/all?origQuery=${keyword}
   &pagingIndex=${pageIndex}
   &pagingSize=80&productSet=total&query=${keyword}
   &sort=rel&timestamp=&viewType=list​`;
   return url;
-}
+};
 
-async function PageScroll(page) {
+const PageScroll = async (page) => {
   await page.evaluate(async () => {
     await new Promise((resolve, reject) => {
       let totalHeight = 0;
@@ -32,52 +32,67 @@ async function PageScroll(page) {
       }, 100);
     });
   });
-}
-async function PageCrawling(page, productClass) {
+};
+
+const PageCrawling = async (page, productClass) => {
   return await page.evaluate((productClass) => {
     const productElements = Array.from(
       document.querySelectorAll(`[class^="${productClass}"]`)
     );
-    const productData = productElements.map((element) => ({
+    const products = productElements.map((element) => ({
       text: element.innerText,
       href: element.querySelector("a")?.href,
     }));
-    return productData;
+    return products;
   }, productClass);
-}
-
-export default async (req, res) => {
-  const keyword = req.query.id;
-  const pageIndex = Number(req.query.pageIndex || 1);
-  const url = UpdateURL(keyword, pageIndex);
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url, { waitUntil: "networkidle0" });
-  await PageScroll(page);
-  const productArr = await PageCrawling(page, "product_item__");
-  await browser.close();
-
-  const response = productArr
-    .filter((item) => {
-      const price = DataProcessingMallPrice(ProductPrice(item.text));
-      const minPrice = DataProcessingMinPrice(item.text);
-      return price.some(({ shop, price }) => price > minPrice * 1.2);
-    })
-    .map((item) => {
-      const detail = item.text;
-      const link = item.href;
-      const price = DataProcessingMallPrice(ProductPrice(detail));
-      const minPrice = DataProcessingMinPrice(detail);
-      const name = ProductDetailName(detail);
-
-      return {
-        detail: detail,
-        link: link,
-        name: name,
-        price: price,
-        minPrice: minPrice,
-      };
-    });
-
-  res.status(200).json(response);
 };
+
+const getResponseData = async (item) => {
+  const [priceData, minPrice, name] = await Promise.all([
+    DataProcessingMallPrice(ProductPrice(item.text)),
+    DataProcessingMinPrice(item.text),
+    ProductDetailName(item.text),
+  ]);
+
+  return {
+    detail: item.text,
+    link: item.href,
+    name: name,
+    price: priceData,
+    minPrice: minPrice,
+  };
+};
+
+const crawlData = async (req, res) => {
+  try {
+    const keyword = req.query.id;
+    const pageIndex = isNaN(req.query.pageIndex)
+      ? 1
+      : Number(req.query.pageIndex);
+    const url = UpdateURL(keyword, pageIndex);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: "networkidle0" });
+    await PageScroll(page);
+    const products = await PageCrawling(page, "product_item__");
+    await browser.close();
+
+    const responses = await Promise.all(
+      products
+        .filter((item) => {
+          const price = DataProcessingMallPrice(ProductPrice(item.text));
+          const minPrice = DataProcessingMinPrice(item.text);
+          return price.some(({ shop, price }) => price > minPrice * 1.2);
+        })
+        .map(getResponseData)
+    );
+
+    res.status(200).json(responses);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export default crawlData;
