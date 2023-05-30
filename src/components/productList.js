808f47a135b0e588e4c7570a230a45d5d6d1c8b6
Shin <shin4895@gmail.com>
2023-05-30 13:06:19 +0900

    useSWR사용

--- src/components/productList.js
+++ src/components/productList.js
@@ -1,3 +1,4 @@
+import useSWR from "swr";
 import { useState, useEffect } from "react";
 import { ProductItem } from "./productItem";
 import Loading from "./loading";
@@ -9,53 +10,24 @@ const StyledPageButton = styled.button`
   width: 150px;
 `;
 
-const fetcher = async (url) => {
-  const res = await fetch(url, {
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
-    },
-  });
-  if (!res.ok) {
-    throw new Error(`An error occurred: ${res.status}`);
-  }
-  const data = await res.json();
-  return data;
-};
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export function ProductList({ search }) {
   const [pageIndex, setPageIndex] = useState(1);
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(`/api/${search}?pageIndex=${pageIndex}`, {
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
-        },
-      });
-      if (!response.ok) {
-        throw new Error(`An error occurred: ${response.status}`);
-      }
-      const data = await response.json();
-      setData(data);
-      setIsLoading(false);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    if (search) {
-      fetchData();
-    }
-  }, [pageIndex, search]);
+  const { data, error, mutate } = useSWR(
+    search ? `/api/${search}?pageIndex=${pageIndex}` : null,
+    fetcher
+  );
 
   const handleNextBTN = () => {
     setPageIndex(pageIndex + 1);
   };
+  useEffect(() => {
+    if (search) {
+      mutate();
+    }
+  }, [pageIndex, mutate, search]);
 
   const handlePrevBTN = () => {
     if (pageIndex > 1) {
@@ -63,20 +35,18 @@ export function ProductList({ search }) {
     }
   };
 
-  if (!search) {
-    return <div>Start your search!</div>; // Prompt the user to start a search
-  }
+  if (error) return <div>Failed to load</div>;
 
-  if (isLoading) {
-    return (
-      <div>
-        <Loading />
-      </div>
-    ); // Show loading message
-  }
-
-  if (!data || data.length === 0) {
-    return <div>No data available</div>; // Show message when there is no data
+  if (!data) {
+    if (!search) {
+      return <div>Start your search!</div>; // Prompt the user to start a search
+    } else {
+      return (
+        <div>
+          <Loading />
+        </div>
+      ); // Show loading message
+    }
   }
 
   return (
