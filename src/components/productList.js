debe16c2f8efe984a9fac8c3664e48a5122269f9
Shin <shin4895@gmail.com>
2023-05-30 10:32:08 +0900

    add user Agent

--- src/components/productList.js
+++ src/components/productList.js
@@ -11,7 +11,12 @@ const StyledPageButton = styled.button`
 `;
 
 const fetcher = async (url) => {
-  const res = await fetch(url);
+  const res = await fetch(url, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
+    },
+  });
   if (!res.ok) {
     throw new Error(`An error occurred: ${res.status}`);
   }
