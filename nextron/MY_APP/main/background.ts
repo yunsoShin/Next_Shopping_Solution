40012b41fe8e90628a727be91f94bebb107d144b
Yunsu <shin4895@gmail.com>
2024-02-14 21:35:29 +0900

    add-새로운 창을 여는 api로 구현하도록 로직구성 변경

--- nextron/MY_APP/main/background.ts
+++ nextron/MY_APP/main/background.ts
@@ -23,44 +23,14 @@ if (isProd) {
   });
 
   if (isProd) {
-    //await mainWindow.loadURL('app://./home')
-    await mainWindow.loadURL("https://speedgo.domeggook.com/");
+    await mainWindow.loadURL("app://./home");
+    // await mainWindow.loadURL("https://speedgo.domeggook.com/");
   } else {
     const port = process.argv[2];
-    //await mainWindow.loadURL(`http://localhost:${port}/home`)
-    await mainWindow.loadURL("https://speedgo.domeggook.com/");
+    await mainWindow.loadURL(`http://localhost:${port}/home`);
+
     mainWindow.webContents.openDevTools();
   }
-  await mainWindow.webContents.executeJavaScript(`
-    const loginLink = document.querySelector('a.a1[href*="mem_formLogin.php"]');
-    if (loginLink) {
-      loginLink.click();
-    } else {
-      console.error('Login link not found');
-    }
-  `);
-  await mainWindow.webContents.executeJavaScript(`
-  const naverLoginLink = document.querySelector('#lSnsLinkNaver a');
-  if (naverLoginLink) {
-    naverLoginLink.click();
-  } else {
-    console.error('Naver login link not found');
-  }
-`);
-  // 기존의 로그인 자동화 코드 아래에 추가
-  // const clickButtonByIndex = async (index) => {
-  //   await mainWindow.webContents.executeJavaScript(`
-  //   (function() {
-  //     const xpath = '/html/body/section/div[2]/div[3]/div[3]/div[3]/button[${index}]';
-  //     const button = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  //     if (button && button.click) {
-  //       button.click();
-  //     } else {
-  //       console.error('버튼 ' + index + '을 찾을 수 없습니다.');
-  //     }
-  //   })();
-  // `);
-  // };
 })();
 
 app.on("window-all-closed", () => {
