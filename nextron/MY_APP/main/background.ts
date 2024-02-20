6fd19f7a0ec8c9eb561dba00f094ec91b5e52a55
Yunsu <shin4895@gmail.com>
2024-02-20 23:19:57 +0900

    add-일렉트론 메인윈도우에서 스피드고 띄우기

--- nextron/MY_APP/main/background.ts
+++ nextron/MY_APP/main/background.ts
@@ -27,10 +27,26 @@ if (isProd) {
     // await mainWindow.loadURL("https://speedgo.domeggook.com/");
   } else {
     const port = process.argv[2];
-    await mainWindow.loadURL(`http://localhost:${port}/home`);
-
+    // await mainWindow.loadURL(`http://localhost:${port}/home`);
+    await mainWindow.loadURL("https://speedgo.domeggook.com/");
     mainWindow.webContents.openDevTools();
   }
+  await mainWindow.webContents.executeJavaScript(`
+    const loginLink = document.querySelector('a.a1[href*="mem_formLogin.php"]');
+    if (loginLink) {
+      loginLink.click();
+    } else {
+      console.error('Login link not found');
+    }
+  `);
+  await mainWindow.webContents.executeJavaScript(`
+  const naverLoginLink = document.querySelector('#lSnsLinkNaver a');
+  if (naverLoginLink) {
+    naverLoginLink.click();
+  } else {
+    console.error('Naver login link not found');
+  }
+`);
 })();
 
 app.on("window-all-closed", () => {
