fb8c89c317687179b590e8de4e503ad525b2c4f3
shin yun soo <91814941+yunsoShin@users.noreply.github.com>
2024-03-04 23:40:19 +0900

    Update background.ts

--- nextron/MY_APP/main/background.ts
+++ nextron/MY_APP/main/background.ts
@@ -24,7 +24,7 @@ if (isProd) {
 
   if (isProd) {
     await mainWindow.loadURL("app://./home");
-    // await mainWindow.loadURL("https://speedgo.domeggook.com/")
+
   } else {
     const port = process.argv[2];
     // await mainWindow.loadURL(`http://localhost:${port}/home`);
